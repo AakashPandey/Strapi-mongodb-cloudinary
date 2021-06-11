9b4c9c4c126fa39a0c656938632936735bb0c6e7
Aakash Pandey <Aakash.pandey@live.com>
2021-06-12 04:10:49 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,4 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: process.env.HEROKU_URL,
+  admin: {
+    auth: {
+      secret: ${process.env.JWT_KEY}
+    }
+  }
 });
