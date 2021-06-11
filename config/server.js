7529d7be7d109cf936c6ab96e84f5120f46199ed
Aakash Pandey <Aakash.pandey@live.com>
2021-06-12 04:43:40 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   url: process.env.HEROKU_URL,
   admin: {
     auth: {
-      secret: process.env.JWT_KEY
-    }
-  }
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
 });
